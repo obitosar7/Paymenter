e0ff2db2cf3b529de751c6c47fdb8e7969be3cad
obitosar7 <obitosar73733@gmail.com>
2026-05-01 20:29:03 +0300

    Delete extensions/Others/VatManager directory

--- extensions/Others/VatManager/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Paymenter\Extensions\Others\VatManager\Http\Controllers\VatManagerController;
-
-Route::middleware(['web', 'auth'])->prefix('admin/extensions/vat-manager')->group(function () {
-    Route::get('/', [VatManagerController::class, 'index'])->name('extensions.vat-manager.index');
-    Route::post('/', [VatManagerController::class, 'update'])->name('extensions.vat-manager.update');
-});
